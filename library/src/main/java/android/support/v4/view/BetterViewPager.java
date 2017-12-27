85bda73d8db77384c7ed9194fd1c3bf0d4589958
paramjot singh <paramjot.singh@clarity-medical.com>
2017-12-27 12:57:15 +0530

    Test

---
+++ library/src/main/java/android/support/v4/view/BetterViewPager.java
@@ -0,0 +1,18 @@
+package android.support.v4.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+
+public class BetterViewPager extends ViewPager {
+
+    public BetterViewPager(Context context) {
+        super(context);
+    }
+
+    public BetterViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+}
